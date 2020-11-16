96d3c9103372949f686a64331a7a6521fe6c8713
Daniel Hernandez <dehernandez1066misena.edu.co>
2020-11-15 20:19:22 -0500

    se crea sitio web con HTML Y CSS

---
+++ html/villancicos.php
@@ -0,0 +1,42 @@
+<!Doctype html>
+<html>
+
+ <head>  
+     <meta charset="utf-8">
+      
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>
+		 Daniel Hernández.
+	 </title>
+        <link rel="stylesheet" type="text/css" href="estilos.css">
+ </head>
+    <body>
+        <header>
+          <img src="./img/la navidad.png" widht="50" height="80" id="header">
+      </header>
+          
+      <nav>  
+          <ul>    
+              <li>
+                    <a href="../index.php" class="botones"> <img src="../img/botoninicio.png" widht="60" height="80" class="iconos"></a>
+             </li>
+
+                <li>
+                    <a href="villancicos.php" class="botones"> <img src="../img/botonvillancicos.jpg" widht="60" height="80" class="iconos"></a>
+              </li>
+                <li>
+                  <a href="videos.php" class="botones"> <img src="../img/botonvideos.jpg" widht="60" height="80" class="iconos"></a>
+              </li>
+
+            </ul>
+      </nav>
+        <section>
+            
+
+        </section>
+
+       
+    </body>
+
+</html>
+
